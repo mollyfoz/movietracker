e8d077db00ebf8b24a58f05b1339aee5e6350da7
Ralph Burnette <rgburnette3@gmail.com>
2017-08-31 14:51:22 -0600

    add card styling

--- app/components/MovieCard/MovieCard.js
+++ app/components/MovieCard/MovieCard.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import './MovieCard.css'
+
 
 const MovieCard = ({ id, title, date, desc, score, image, fav }) => {
 
   return (
-    <div className='movie-cards'>
-      <h2>{ title }</h2>
-      <p>{ date }</p>
-      <p>{ desc }</p>
-      <img src={`https://image.tmdb.org/t/p/w500${image}`} />
+    <div
+     className='movie-cards'>
+     <div className='overlay'>
+      <h2 >{ title }</h2>
+      <p className='card-text'>{ date }</p>
+      <p className='card-text'>{ desc }</p>
+      <img className='movie-img' src={`https://image.tmdb.org/t/p/w500${image}`} />
       <p>{ score }</p>
+      </div>
     </div>
   )
 }
