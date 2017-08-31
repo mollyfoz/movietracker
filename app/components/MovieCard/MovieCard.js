c066cfb8d187c669f07e0dc288a04685b5f0faab
Ralph Burnette <rgburnette3@gmail.com>
2017-08-31 15:35:11 -0600

    complete hidden overlay

--- app/components/MovieCard/MovieCard.js
+++ app/components/MovieCard/MovieCard.js
@@ -6,13 +6,11 @@ const MovieCard = ({ id, title, date, desc, score, image, fav }) => {
   return (
     <div
      className='movie-cards'>
-     <div className='overlay'>
-      <h2 >{ title }</h2>
-      <p className='card-text'>{ date }</p>
-      <p className='card-text'>{ desc }</p>
+      <h2 className='card-title-text text' >{ title }</h2>
+      <p className='date-text text'>{ date }</p>
+      <p className='card-text text'>{ desc }</p>
+      <p className='score-text text'>{ score }</p>
       <img className='movie-img' src={`https://image.tmdb.org/t/p/w500${image}`} />
-      <p>{ score }</p>
-      </div>
     </div>
   )
 }
